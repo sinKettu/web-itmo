e8bd0528a6293a7f378cabccaa4a34ea5cd4b865
SinFox <avangard.jazz@gmail.com>
2020-03-21 23:10:48 +0300

    JQ Task 4: done

--- jquery/task4.js
+++ jquery/task4.js
@@ -1,9 +1,47 @@
 function first()
 {
-    $("#id1").load("https://inxaoc.github.io/example/ajax-1.html")
+    $("#id1").load("https://inxaoc.github.io/example/ajax-1.html");
+}
+
+function getList(depth, json)
+{
+    if (depth >= 10)
+    {
+        return "ERROR";
+    }
+
+    //let tab = "\t".repeat(depth);
+    let result = "<ul>";
+    for (let key in json)
+    {
+        let val = json[key];
+        if(typeof val === "object")
+        {
+            result += "<li>" + key;
+            let ret = getList(depth + 1, val);
+            if (ret == "ERROR")
+            {
+                return "ERROR";
+            }
+            result += ret + "</li>";
+        }
+        else
+        {
+            result += "<li>" + key + ": " + val + "</li>"
+        }
+    }
+    result += "</ul>";
+    return result;
+}
+
+function parseJson(resp, status, xhr)
+{
+    let json = JSON.parse(resp);
+    let lst = getList(0, json);
+    $("#id2").html(lst);
 }
 
 function second()
 {
-
+    $("#id2").load("https://inxaoc.github.io/example/ajax.json?param1=111", parseJson);
 }
